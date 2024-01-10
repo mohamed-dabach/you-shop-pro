d67ae1bf4c56c208da9e05701317475a54f8eae4
user912e <dftardftar130@gmail.com>
2024-01-11 00:29:15 +0100

    style: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // custom colors
+        primary: "#30c7b5",
+        secondary: "#00ac97",
+        "semi-white": "#f3f6f3",
+        "semi-black": "#14261c",
+        "semi-gray": "#4f5655",
+      },
+    },
   },
   plugins: [],
 };
