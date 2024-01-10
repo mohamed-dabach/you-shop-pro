9f8ce3ead6d6a0b9259f30101ffa83ce58d73037
user912e <dftardftar130@gmail.com>
2024-01-10 22:52:58 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-400 text-2xl">dach tash git bash 9ash</h1>
+      <h1 className="text-red-400 text-2xl">dach tash git bash 9ash med</h1>
     </>
   );
 }
