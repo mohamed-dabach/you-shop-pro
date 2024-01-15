09de15850bffcda1d6f7201212bfd1406f52e2de
Abde-Ouaaddi29 <abderrahimouaaddi371@gmail.com>
2024-01-15 02:22:49 +0100

    footer

---
+++ src/Components/footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+
+
+    return (<>
+        <footer>
+            {/* footer top */}
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            {/* footer bottom */}
+            <div>
+
+            </div>
+        </footer>
+    </>)
+}
