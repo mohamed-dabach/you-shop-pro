9ba54e307b297f47bbad5a3f1a6d1596ea36a2da
user912e <dftardftar130@gmail.com>
2024-01-16 13:19:20 +0100

    working on product page side filters

---
+++ src/Components/SideProductFilter.jsx
@@ -0,0 +1,76 @@
+import Slider from "@mui/material/Slider";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+const SideProductFilters = ({ handleToggleFilters }) => {
+  const [maxMinPrice, setMaxMinPrice] = useState([20, 37]);
+  return (
+    <>
+      <div
+        onClick={handleToggleFilters}
+        className="fixed top-0 z-[98] opacity-[.5] left-0 w-full h-full bg-semi-gray"
+      ></div>
+      <div className="fixed bg-white pt-[60px] px-5 top-0 z-[99] left-0 h-full w-[300px]">
+        <button
+          onClick={handleToggleFilters}
+          className="absolute  top-4 right-5"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-6 h-6  "
+          >
+            <path d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </button>
+        <div className="flex">
+          <input
+            type="text"
+            className=" ps-2 border"
+            placeholder="Search Product"
+          />
+          <button className="uppercase border bg-primary text-sm p-2 tracking-wide font-semibold">
+            Search
+          </button>
+        </div>
+        <div className="py-3 text-xl font-semibold ">
+          <h4 className="mb-5">Filter by price</h4>
+          <RangeSlider
+            maxMinPrice={maxMinPrice}
+            setMaxMinPrice={setMaxMinPrice}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+function RangeSlider({ maxMinPrice, setMaxMinPrice }) {
+  const handleChange = (event, newValue) => {
+    setMaxMinPrice(newValue);
+  };
+
+  console.log(maxMinPrice);
+
+  return (
+    <Slider
+      getAriaLabel={() => "Temperature range"}
+      value={maxMinPrice}
+      onChange={handleChange}
+      valueLabelDisplay="off"
+      disableSwap
+    />
+  );
+}
+
+SideProductFilters.propTypes = {
+  handleToggleFilters: PropTypes.func.isRequired,
+};
+
+RangeSlider.propTypes = {
+  maxMinPrice: PropTypes.number.isRequired,
+  setMaxMinPrice: PropTypes.func.isRequired,
+};
+export default SideProductFilters;
