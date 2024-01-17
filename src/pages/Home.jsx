59b1fda5f1d390c9f891d5553567e72820b2cdfa
user912e <dftardftar130@gmail.com>
2024-01-17 17:58:27 +0100

    Fix bug in responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,12 @@ import HomeHeader from "../Components/HomeHeader";
 import WhatPeopleSay from "../Components/WhatPeopleSay";
 import HomeMostSallingProduct from "../Components/HomeMostSallingProduct";
 
-
 const Home = () => {
   return (
     <>
       <HomeHeader />
-      <WhatPeopleSay />
       <HomeMostSallingProduct />
+      <WhatPeopleSay />
     </>
   );
 };
