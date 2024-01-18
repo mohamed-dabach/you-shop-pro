6b72607e57e1072c9cab071f1e0dc54e9dffa6ca
user912e <dftardftar130@gmail.com>
2024-01-18 17:41:00 +0100

    finiched rooms page

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,11 +1,11 @@
 import RoomComponent from "../Components/roomComponent";
-
+import SeeLastCol from "../Components/SeeLastCol"
 const Rooms = () => {
   const rooms = [
     {
       id: 1,
       room: "Living Room",
-      img: "/rooms/bedroom-img.jpg",
+      img: "/src/Assets/rooms/living-room-img.jpg",
       disc: "Massa cras egestas laoreet montes, dapibus eu sit etiam curabitur faucibus habitasse lectus vestibulum leo, odio dolor quis maecenas faucibus vulputate pharetra.",
       items: [
         "Massa cras egestas laoreet montes",
@@ -19,41 +19,95 @@ const Rooms = () => {
         "Aliquet dignissim",
       ],
     },
-    // {
-    //   id: 2,
-    //   room: "Bedroom",
-    //   img: "/rooms/bedroom-img.jpg",
-    //   items: [
-    //     "Massa cras egestas laoreet montes",
-    //     "dapibus eu sit etiam curabitur",
-    //     "faucibus habitasse lectus vestibulum leo",
-    //     "odio dolor quis maecenas faucibus vulputate pharetra",
-    //     "Nulla placerat viverra",
-    //     "Cursus viverra",
-    //     "Vitae interdum eget",
-    //     "Risus tempus elementum",
-    //     "Aliquet dignissim",
-    //   ],
-    // },
+    {
+      id: 2,
+      room: "Beth room",
+      disc: "Massa cras egestas laoreet montes, dapibus eu sit etiam curabitur faucibus habitasse lectus vestibulum leo, odio dolor quis maecenas faucibus vulputate pharetra.",
+      img: "/src/Assets/rooms/bath-room-img.jpg",
+      items: [
+        "Massa cras egestas laoreet montes",
+        "dapibus eu sit etiam curabitur",
+        "faucibus habitasse lectus vestibulum leo",
+        "odio dolor quis maecenas faucibus vulputate pharetra",
+        "Nulla placerat viverra",
+        "Cursus viverra",
+        "Vitae interdum eget",
+        "Risus tempus elementum",
+        "Aliquet dignissim",
+      ],
+    },
+    {
+      id: 3,
+      room: "Bedroom",
+      disc: "Massa cras egestas laoreet montes, dapibus eu sit etiam curabitur faucibus habitasse lectus vestibulum leo, odio dolor quis maecenas faucibus vulputate pharetra.",
+      img: "/src/Assets/rooms/bedroom-img.jpg",
+      items: [
+        "Massa cras egestas laoreet montes",
+        "dapibus eu sit etiam curabitur",
+        "faucibus habitasse lectus vestibulum leo",
+        "odio dolor quis maecenas faucibus vulputate pharetra",
+        "Nulla placerat viverra",
+        "Cursus viverra",
+        "Vitae interdum eget",
+        "Risus tempus elementum",
+        "Aliquet dignissim",
+      ],
+    },
+    {
+      id: 4,
+      room: "kitcheen",
+      disc: "Massa cras egestas laoreet montes, dapibus eu sit etiam curabitur faucibus habitasse lectus vestibulum leo, odio dolor quis maecenas faucibus vulputate pharetra.",
+      img: "/src/Assets/rooms/kitchen-img.jpg",
+      items: [
+        "Massa cras egestas laoreet montes",
+        "dapibus eu sit etiam curabitur",
+        "faucibus habitasse lectus vestibulum leo",
+        "odio dolor quis maecenas faucibus vulputate pharetra",
+        "Nulla placerat viverra",
+        "Cursus viverra",
+        "Vitae interdum eget",
+        "Risus tempus elementum",
+        "Aliquet dignissim",
+      ],
+    },
+    {
+      id: 5,
+      room: "Home Office",
+      disc: "Massa cras egestas laoreet montes, dapibus eu sit etiam curabitur faucibus habitasse lectus vestibulum leo, odio dolor quis maecenas faucibus vulputate pharetra.",
+      img: "/src/Assets/rooms/home-office-img.jpg",
+      items: [
+        "Massa cras egestas laoreet montes",
+        "dapibus eu sit etiam curabitur",
+        "faucibus habitasse lectus vestibulum leo",
+        "odio dolor quis maecenas faucibus vulputate pharetra",
+        "Nulla placerat viverra",
+        "Cursus viverra",
+        "Vitae interdum eget",
+        "Risus tempus elementum",
+        "Aliquet dignissim",
+      ],
+    },
   ];
 
   return (
-    <>
+    <div>
       <div className="container">
-        <div className="flex justify-center items-center mb-5 flex-col m-auto text-center max-w-[700px]">
-          <h1 className="text-5xl font-semibold mb-5">Rooms</h1>
-          <p>
+        <div className="flex justify-center items-center mb-5  mt-10 flex-col m-auto text-center max-w-[700px]">
+          <h1 className="text-7xl font-semibold my-5 text-semi-black">Rooms</h1>
+          <p className="text-semi-black text-lg mb-10">
             Vestibulum, diam vulputate amet cras in diam quis turpis curabitur
             tellus aliquet tellus iaculis tempus, sollicitudin massa duis
             eleifend egestas turpis sit etiam.
           </p>
         </div>
-        {rooms &&
-          rooms.map((room) => {
-            return <RoomComponent key={room.id} room={room} />;
-          })}
       </div>
-    </>
+      {rooms &&
+        rooms.map((room) => {
+          return <RoomComponent key={room.id} room={room} />;
+        })}
+
+      <SeeLastCol />
+    </div>
   );
 };
 
