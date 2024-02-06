fd5e8210d9c326ee80208e799f8853acf943c334
said <saidkourbisse@gmail.com>
2024-02-06 01:13:22 +0100

    v1.0 hh <said>

--- src/reducers/action.js
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux"
-const dispatch=useDispatch()
-export const deleteorder=(val)=>{
-    dispatch({
-        type: "DELETE_ORDER",
-        payload: val
-      })
-}
